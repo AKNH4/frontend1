de4031eadf11b078d2e3bfb554583f607e515274
AKNH4 <1234a7q@gmail.com>
2022-07-04 14:57:30 +0200

    Kontodetails hinzugefügt

---
+++ src/app/private/account/user-details/user-details.component.ts
@@ -0,0 +1,13 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { UserService } from 'src/app/core/services/user.service';
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.scss'],
+})
+export class UserDetailsComponent implements OnInit {
+  constructor(public userService: UserService) {}
+
+  ngOnInit(): void {}
+}
